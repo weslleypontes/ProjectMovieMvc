8f185ce4c7e18c7f660254345d29be3b34c5904a
Weslley Pontes <contato.weslleypontes@gmail.com>
2023-05-26 13:02:33 -0300

    Adicionado pesquisa por gênero

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -21,12 +21,18 @@ namespace MvcMovie.Controllers
 
         // GET: Movies
         //adiciona a funcionalidade de pesquisa ao método de ação Index que permite pesquisar filmes por gênero ou nome
-        public async Task<IActionResult> Index(string searchString)
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
             if (_context.Movie == null)
             {
                 return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
             }
+
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+            
             //A linha a seguir do método de ação Index cria uma consulta LINQ para selecionar os filmes
             var movies = from m in _context.Movie
                          select m;
@@ -36,7 +42,18 @@ namespace MvcMovie.Controllers
                 movies = movies.Where(s => s.Title!.Contains(searchString));
             }
 
-            return View(await movies.ToListAsync());
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+
+            return View(movieGenreVM);
         }
 
         [HttpPost]
---
+++ MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MvcMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+
+        public List<Movie>? Movies { get; set; }
+        //Uma SelectList que contém a lista de gêneros. Isso permite que o usuário selecione um gênero na lista
+        public SelectList? Genres { get; set; }
+        public string? MovieGenre { get; set; }
+        public string? SearchString { get; set; }
+    }
+}
