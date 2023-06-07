f5d193cbc62c94dbfa3ce314c9ac0b4532b48ef5
Weslley Pontes <contato.weslleypontes@gmail.com>
2023-06-07 15:58:54 -0300

    Adicionando novas annotations

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,22 +1,42 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MvcMovie.Models;
 
 public class Movie
 {
     public int Id { get; set; }
+
+    [StringLength(60, MinimumLength = 3, ErrorMessage = "Tamanho do {0} deve ser entre {2} e {1}.")]
+    [Required(ErrorMessage = "{0} é obrigatório.")]
     public string? Title { get; set; }
 
+    [Required(ErrorMessage = "{0} é obrigatório.")]
     [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
-    public string? Genre { get; set; }
 
+    [Required(ErrorMessage = "{0} é obrigatório")]
+    [RegularExpression(@"^[0-9]*$", ErrorMessage = "O campo {0} deve ser um número.")]
+    [Range(1, 100, ErrorMessage = "O {0} deve ser de {1} até {2}")]
+    [DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
-    public string Rating { get; set; }
 
-    public Movie()
+
+    [Required(ErrorMessage = "{0} é obrigatório.")]
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$", ErrorMessage = "Use apenas letras.\r\nA primeira letra deve ser maiúscula.")]
+    [StringLength(30)]
+    public string? Genre { get; set; }
+
+
+    [Required(ErrorMessage = "{0} é obrigatório.")]
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$", ErrorMessage = "O campo {0} deve corresponder à expressão regular {1}")]
+    [StringLength(5)]
+    public string? Rating { get; set; }
+
+public Movie()
     {
     }
 
